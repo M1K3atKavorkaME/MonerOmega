4a43a3676878101b6a3c28fbd240723237b56077
M1K3atKavorkaME <bs0d.s0b7@gmail.com>
2017-10-02 07:55:52 -0230

    Update oledMoneroDisplay.py

--- oledMoneroDisplay.py
+++ oledMoneroDisplay.py
@@ -7,17 +7,6 @@ from OmegaExpansion import oledExp
 
 baseUrl     = "https://cnhv.co/28z0"
 
-def oledWriteMonero(text, date):
-    if oledExp.driverInit() != 0:
-        print 'ERROR: Could not initialize the OLED Expansion'
-        return False
-
-    # set the cursor to the next line
-    oledExp.setCursor(1,0)
-
-    # write out the monero
-    oledExp.write(text)
-
 ### MAIN PROGRAM ###
 def print 'Got monero! ', monero
     # display the monero on the OLED
